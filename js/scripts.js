2ef1e10a93c7823addead81355e617f4ff96142e
Yelena Nedelko & Kendra Ash <student@epicodus.com>
2015-09-10 11:36:22 -0700

    return correct roman numbers up to 19.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,30 @@
 var romanNumeral = function(number) {
   var output = [];
+  if (number >= 9 && number <= 19) {
+    if (number > 9) {
+       output.push("X");
+       number = number%10;
+    }
+    if (number === 9) {
+       output.push("I", "X");
+       number = number - number;
+    }
+  }
   if(number >= 4 && number < 9) {
+    if(number === 4) {
+      output.push("I", "V");
+      number = number - number;
+    }
+    else {
     output.push("V");
     number = number%5;
+    }
   }
   if (number < 4) {
     for (var i=0; i < number; i++) {
       output.push("I");
     }
   }
+
   return output;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,16 @@ describe("romanNumeral", function() {
   it("returns the correct roman numeral up to 8", function() {
     expect(romanNumeral(8)).to.eql(['V', 'I', 'I','I']);
   });
+  it("returns the correct roman numeral for 4", function() {
+    expect(romanNumeral(4)).to.eql(['I','V']);
+  });
+  it("returns the correct roman numeral for 9", function() {
+    expect(romanNumeral(9)).to.eql(['I','X']);
+  });
+  it("returns the correct roman numeral for 14", function() {
+    expect(romanNumeral(14)).to.eql(['X','I','V']);
+  });
+  it("returns the correct roman numeral for 19", function() {
+    expect(romanNumeral(19)).to.eql(['X',"I","X"]);
+  });
 });
