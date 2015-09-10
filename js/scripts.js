da8ebbb7dc87910905c2fa38695ccd4781234c5e
Yelena Nedelko & Kendra Ash <student@epicodus.com>
2015-09-10 11:11:19 -0700

    returns the correct roman numberal up to 8

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 var romanNumeral = function(number) {
   var output = [];
-
-  if (number < 5) {
+  if(number >= 4 && number < 9) {
+    output.push("V");
+    number = number%5;
+  }
+  if (number < 4) {
     for (var i=0; i < number; i++) {
       output.push("I");
     }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe("romanNumeral", function() {
-  it("returns the correct roman numberal up to 4", function() {
-    expect(romanNumeral(4)).to.eql(['I','I','I','I']);
+  it("returns the correct roman numeral up to 3", function() {
+    expect(romanNumeral(3)).to.eql(['I','I','I']);
+  });
+  it("returns the correct roman numeral for 5", function() {
+    expect(romanNumeral(5)).to.eql(['V']);
+  });
+  it("returns the correct roman numeral up to 8", function() {
+    expect(romanNumeral(8)).to.eql(['V', 'I', 'I','I']);
   });
 });
